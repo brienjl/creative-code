5d5f2291783df4fe205e9fb1f9b8ee76f46282c2
Jeremy <brienjl@gmail.com>
2022-09-25 21:39:13 -0700

    project radius uploads

--- sketch-radius.js
+++ sketch-radius.js
@@ -1,23 +1,33 @@
 const canvasSketch = require('canvas-sketch');
 
 const settings = {
-  dimensions: [ 1080, 1080 ]
+  dimensions: [ 1080, 1080 ],
+  pixelsPerInch: 300
 };
 
 //utility functions
-const degToRad = (degrees) => {
+function degToRad(degrees) {
   return degrees / 180 * Math.PI;
-};
+}
 
-const randomRange = (min, max) => {
-  return Math.random() * (max - min) + min
-};
+function randomRange(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+function randomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = "#"
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
 
 const sketch = () => {
   return ({ context, width, height }) => {
-    context.fillStyle = 'white';
+    context.fillStyle = randomColor();
     context.fillRect(0, 0, width, height);
-    context.fillStyle = 'black';
+    context.fillStyle = randomColor();
     const cx = width * 0.5;
     const cy = height * 0.5;
     const w = width * 0.01;
@@ -25,6 +35,10 @@ const sketch = () => {
     let x, y;
     const num = 12;
     const radius = width * 0.3;
+    
+    console.log(context.fillStyle)
+
+    
     
     //draw the clockface
     for (let i = 0; i < num; i++) {
@@ -37,12 +51,13 @@ const sketch = () => {
       context.save();
       context.translate(x,y);
       context.rotate(-angle);
-      context.scale(Math.random() * (3.5-1) + .25, 1);
+      context.scale(Math.random() * (7.5-1) + .25, 1);
 
       context.beginPath();
       context.rect(-w * 0.5, -h * 0.5, w, h);
       context.fill();
       context.restore();
+
     }
   };
 };
